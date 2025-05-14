de982f0578f6295bd8912aa8b5684470c28c7180
Phantom-AL <den2005den0715@gmail.com>
2025-05-14 15:13:37 +0300

    test-deploy

--- film_base/settings.py
+++ film_base/settings.py
@@ -91,10 +91,16 @@ WSGI_APPLICATION = 'film_base.wsgi.application'
 # }
 
 import dj_database_url
+import os
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://localhost:5432/postgres', conn_max_age=600)
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', 'postgres://localhost:5432/postgres'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 
 # Password validation
@@ -157,3 +163,8 @@ CACHES = {
         'LOCATION': os.path.join(BASE_DIR, 'cinema_cache'),
     }
 }
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
